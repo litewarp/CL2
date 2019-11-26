e02a4042ec2513307cf79aafba9d3d0b834a9d5c
Nick Sethi <nicholas.sethi@gmail.com>
2019-11-26 15:22:14 -0800

    added razzle-heroku plugin, modified webpack

--- razzle.config.js
+++ razzle.config.js
@@ -1,1 +1,11 @@
-module.exports = { plugins: ["typescript"] };
+'use strict'
+
+module.exports = {
+  plugins: ["typescript"],
+  modify: (config, { target, dev }, webpack) => {
+    // stay immutable
+    const newConfig = razzleHeroku(config, { target, dev}, webpack)
+
+    return newConfig
+  }
+};
