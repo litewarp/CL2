d9cf5df0feb045ae2da0f4f069986548a3a58842
Nick Sethi <nicholas.sethi@gmail.com>
2019-11-26 15:51:57 -0800

    added razzle-heroku plugin, modified webpack
    again
    
    ias:
    
    procfile
    
    razzleheroku
    
    razzleheroku
    
    razzleheroku
    
    razzleheroku
    
    razzleheroku
    
    razzleheroku

--- razzle.config.js
+++ razzle.config.js
@@ -1,1 +1,13 @@
-module.exports = { plugins: ["typescript"] };
+'use strict'
+
+const razzleHeroku = require('razzle-heroku')
+
+module.exports = {
+  plugins: ["typescript"],
+  modify: (config, { target, dev }, webpack) => {
+    // stay immutable
+    const newConfig = razzleHeroku(config, { target, dev}, webpack)
+
+    return newConfig
+  }
+};
