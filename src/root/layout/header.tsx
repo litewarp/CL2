814fae30a78ec60b83a08e46d59ecbb71747d134
Nick Sethi <nick@sethipc.com>
2019-11-28 09:40:22 -0800

    use themeContext to control layout state

--- src/root/layout/header.tsx
+++ src/root/layout/header.tsx
@@ -1,8 +1,6 @@
 import { Anchor, Box, CheckBox, Heading } from 'grommet'
 import * as React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
 import styled from 'styled-components'
-import { ApplicationState } from '../../typings'
 // local Components
 const Banner = styled(Heading)` font-family: 'Roboto Slab'`
 const FreeLawLink = <Anchor href="https://free.law" label="Free Law Project" color="accent-1"/>
@@ -11,39 +9,32 @@ const HeaderAnchor = (props: { href: string, label: string }) => (
   <Anchor {...props} margin="small" color="dark-5" size="small"/>
 )
 
-export default () => {
-  // get data from the redux store
-  // pass useSelector a getter function and an optional equality function
-  const darkMode = useSelector((state: ApplicationState) => state.layout.darkMode)
-  // dispatch actions from the store using useDispatch
-  const dispatch = useDispatch()
-  return (
-    <>
-      <Box direction="column" margin={{ right: 'auto'}} basis="1/2">
-        <Box direction="row">
-          <Banner level={1} margin="none" color="neutral-2">Court</Banner>
-          <Banner level={1} margin="none" color="accent-2">Listener</Banner>
-        </Box>
-        <Heading level={6} size="small" margin="none">
-          From {FreeLawLink}, a 501(c)(3) corporation
-        </Heading>
+export default ({ darkMode, toggleDarkMode }: { darkMode: boolean, toggleDarkMode: () => void}) => (
+  <>
+    <Box direction="column" margin={{ right: 'auto'}} basis="1/2">
+      <Box direction="row">
+        <Banner level={1} margin="none" color="neutral-2">Court</Banner>
+        <Banner level={1} margin="none" color="accent-2">Listener</Banner>
       </Box>
-      <Box direction="column" align="end" basis="1/2">
-        <Box direction="row" pad="small">
-          <HeaderAnchor href="/about" label="About" />
-          <HeaderAnchor href="/faq" label="FAQ" />
-          <HeaderAnchor href="#" label="Tour" />
-          <HeaderAnchor href="/sign-in" label="Sign in / Register" />
-        </Box>
-        <CheckBox
-          reverse
-          toggle
-          color="dark-5"
-          checked={darkMode}
-          label={<Anchor margin="none" label={`Toggle ${ darkMode ? 'Light' : 'Dark' } Mode`} color="dark-5"/>}
-          onChange={() => ({ action: 'fire hook in withLayout' })}
-        />
+      <Heading level={6} size="small" margin="none">
+        From {FreeLawLink}, a 501(c)(3) corporation
+      </Heading>
+    </Box>
+    <Box direction="column" align="end" basis="1/2">
+      <Box direction="row" pad="small">
+        <HeaderAnchor href="/about" label="About" />
+        <HeaderAnchor href="/faq" label="FAQ" />
+        <HeaderAnchor href="#" label="Tour" />
+        <HeaderAnchor href="/sign-in" label="Sign in / Register" />
       </Box>
-    </>
-  )
-}
+      <CheckBox
+        reverse
+        toggle
+        color="dark-5"
+        checked={darkMode}
+        label={<Anchor margin="none" label={`Toggle ${ darkMode ? 'Light' : 'Dark' } Mode`} color="dark-5"/>}
+        onChange={() => toggleDarkMode()}
+      />
+    </Box>
+  </>
+)
--- src/root/layout/withLayout.tsx
+++ src/root/layout/withLayout.tsx
@@ -1,10 +1,10 @@
 // withLayout.tsx - layout high-order-component
-// wraps the passed in component with the css layout
+// wraps the provided component with the css layout
+// and injects layout-related props
 
 import { Box, Grid, Grommet, ResponsiveContext } from 'grommet'
 import * as React from 'react'
 import { useSelector } from 'react-redux'
-import styled from 'styled-components'
 import { Footer, FooterIcons } from './footer'
 import Header from './header'
 import OptionBar from './options'
@@ -13,55 +13,57 @@ import theme from './theme'
 // set max-width of main content container to 1280px;
 const ThemeContext = React.createContext({
   darkMode: true,
-  toggleDarkMode: () => ({}),
+  toggleDarkMode: () => {}
 })
 
-const ThemeProvider = (props: { children: React.ReactNode }) => {
-  const localStorageState = window.localStorage.getItem('darkMode')
-  const [ darkMode, setDark ] = React.useState(window.localStorage.getItem('darkMode'))
+const ThemeProvider = (props: { children: React.ReactChildren }) => {
+  // do we have a stored theme in localStorage? If so, use it to set the initial state.
+  const locallyStoredTheme = window.localStorage.getItem('darkMode') ? true : false
+  // create the state as a boolean and create callback
+  const [ darkMode, setDark ] = React.useState(locallyStoredTheme)
 
+  // hook to determine whether to re-render when props change
   React.useLayoutEffect(
     () => {
-      const storedThemeState = window.localStorage.getItem('darkMode')
-
+      const localTheme = window.localStorage.getItem('darkMode')
       // if lastTheme set in localStorage, use it
-      storedThemeState ? setDark(true) : setDark(false)
-
-      // render the new theme only when the dark prop changes
+      localTheme && localTheme !== 'false'
+        ? setDark(true)
+        : setDark(false)
     },
+    // render the new theme only when the dark state changes
     [darkMode]
   )
 
   const toggleDarkMode = () => {
+    // change local state
     setDark(!darkMode)
-    window.localStorage.setItem('darkMode', !darkMode)
-  }
+    // localStorage does not accept boolean values
+    // so the presence of a string other than false acts as a boolean
+    !darkMode === true
+      ? window.localStorage.setItem('darkMode', 'TRUE')
+      : window.localStorage.setItem('darkMode', 'FALSE')
+    }
+  // create the new context prop and return it with callback
 
-  // return a component with the prop value and the function to modify it
   return (
-    <ThemeContext.Provider value=({
+    <ThemeContext.Provider
+      value={{
         darkMode,
         toggleDarkMode
-      })
+      }}
     >
-      {props.children}
-    </ ThemeContext.Provider>
+        {props.children}
+    </ThemeContext.Provider>
   )
-
 }
-
-
-
-
 
 export default (Component: React.ComponentType<{props: any}>) => (...props: any) => {
   // inject size prop from responsive context provider
   const size = React.useContext(ResponsiveContext)
   // inject darkMode flag from layout state
-  const darkMode = useSelector(
-    ({ layout }: { layout: { darkMode: boolean }}) => layout.darkMode
-)
-  // Apply theme and pass responsive size prop to the PageLayout
+  //  Apply theme and pass responsive size prop to the PageLayout
+  const { darkMode, toggleDarkMode } = React.useContext(ThemeContext)
   return (
     <Grommet theme={theme} themeMode={darkMode ? 'dark' : 'light'} full>
       <Box
@@ -72,7 +74,7 @@ export default (Component: React.ComponentType<{props: any}>) => (...props: any)
         gridArea="header"
         background={darkMode ? 'dark-2' : 'light-4'}
       >
-        <Header/>
+        <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
       </Box>
       <OptionBar/>
       <Box gridArea="content" pad="medium">
